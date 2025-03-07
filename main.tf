5556f29b8ace0f2c59eea653595095754a5815ff
Philipp G. <philipp.glaw@gmx.de>
2025-03-07 17:23:25 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ resource "aws_vpc" "dev_vpc" {
 resource "aws_subnet" "public-1" {
   vpc_id     = aws_vpc.dev_vpc.id
   cidr_block = "10.0.1.0/24"
-  availability_zone = "us-east-1a"
+  availability_zone = "us-west-2a"
   map_public_ip_on_launch = true
 
   tags = {
