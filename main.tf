4eb5c3e429d11f445c379db869504e56d2982315
Philipp G. <philipp.glaw@gmx.de>
2025-03-07 17:19:37 +0100

    Update main.tf
    Add subnet

--- main.tf
+++ main.tf
@@ -8,3 +8,14 @@ resource "aws_vpc" "dev_vpc" {
     name     = "deham9"
   }       
 }
+
+resource "aws_subnet" "public-1" {
+  vpc_id     = aws_vpc.dev_vpc.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "deham9"
+  }
+}
